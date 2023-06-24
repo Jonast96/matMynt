1e01537451865e35a5c6f0b2f827e65c37dbe58b
Jonast96 <Jon.Astveit@hotmail.com>
2023-06-24 16:08:19 +0200

    changed design

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,10 @@ export default {
   theme: {
     extend: {},
     colors: {
-      primary: "#f0f5eb",
-      secondary: "#324c62",
-      accent: "#62324c",
-      white: "#33333",
+      primary: "#101119",
+      secondary: "#656d9a",
+      accent: "#5b628b",
+      white: "#ecedf3",
     },
   },
   plugins: [],
