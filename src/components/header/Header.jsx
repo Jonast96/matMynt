ccdebd7564e8925ba479ef7e37fece963aae4ed3
Jonast96 <Jon.Astveit@hotmail.com>
2023-06-29 13:53:16 +0200

    added search functionality, need to update searchResults list

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,33 @@
-import { set } from "date-fns";
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
+import { debounce } from "lodash";
 
 function Header() {
   const [search, setSearch] = useState("");
   const [dropdownVisible, setDropdownVisible] = useState(false);
+  const [searchResults, setSearchResults] = useState([]);
+  const key = "NBT65Ohkkh5oIVNrbAfFuEO6ftZzZhzHWDdsRXNb";
 
-  const tempData = [
-    "Apple",
-    "Banana",
-    "Carrot",
-    "Donut",
-    "Eggs",
-    "Fish",
-    "Grapes",
-  ];
+  const fetchData = async (search) => {
+    const response = await fetch(
+      `https://kassal.app/api/v1/products?search=${search}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${key}`,
+        },
+      }
+    );
+    const data = await response.json();
+    setSearchResults(data.data);
+    console.log(data.data);
+  };
+  const debounceFetchData = useCallback(debounce(fetchData, 500), []);
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log(search);
+  function handleChange(e) {
+    const { value } = e.target;
+    setSearch(value);
     setDropdownVisible(true);
+    debounceFetchData(value);
   }
 
   return (
@@ -32,7 +41,7 @@ function Header() {
       </div>
       <form
         onSubmit={(e) => {
-          handleSubmit(e);
+          e.preventDefault();
         }}
         className="searchContainer flex justify-center gap-2  mx-auto mt-3"
       >
@@ -42,7 +51,7 @@ function Header() {
             name="search"
             id="search"
             value={search}
-            onChange={(e) => setSearch(e.target.value)}
+            onChange={handleChange}
             placeholder="Søk etter produkt..."
             className=" bg-primary p-1 text-lg   text-white border-solid border-b-2 border-white focus:outline-none focus:border-accent"
           />
@@ -53,7 +62,7 @@ function Header() {
           {dropdownVisible && (
             <div className="relative">
               <div className="absolute z-10  w-full bg-accent text-black shadow-lg  py-2">
-                {tempData.map((item, index) => (
+                {searchResults.map((item, index) => (
                   <div
                     onClick={() => {
                       setSearch("");
@@ -62,7 +71,7 @@ function Header() {
                     key={index}
                     className="px-4 py-2 hover:bg-gray-200 cursor-pointer"
                   >
-                    {item}
+                    {item.name}
                   </div>
                 ))}
               </div>
